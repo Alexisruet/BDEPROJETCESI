8e57bc965a726a12f8d68112dfe53854642f0d40
Alexisruet <alexis.ruet@viacesi.fr>
2019-01-18 09:46:54 +0100

    ajout idée

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/idea', function () {
+    return view('page/createidea');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('connected');
+//Route::resource('/idea', 'IdeaBoxController@create');
+
+
+Route::middleware(['admin'])->group(function(){
+    Route::get('/admin/gestion', 'GestionController@index')->name('gestion');
+    Route::post('/admin/addEvent', 'EventController@create')->name('addEvent');
+    Route::post('/admin/addProduct', 'ProductController@create')->name('addProduct');
+
+});
+
+Route::middleware(['salariés'])->group(function(){
+});
+
+Route::middleware(['étudiants'])->group(function(){
+    Route::post('/étudiants/addIdea', 'IdeaController@create')->name('addIdea');
+
+});
+
+
+   
+
