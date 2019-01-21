2b1336465d08abd66cab3275000585bbb66fb39b
LouisPouget <louis.pouget@viacesi.fr>
2019-01-21 17:54:20 +0100

    Bon y'a encore des bails mais bon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -80,69 +80,69 @@
 					</div>
 				</div>
 			</nav>
-	@yield('background')
 		</header>
 		<main>
-			@yield('main')
+			<div id="main">
+							@yield('main')
+			</div>
 		</main>
 
-
-	<footer  class="row">
+	<footer>
 		<div class="col-lg-12">
-				<nav class="navbar fixed-bottom navbar-expand-lg navbar-dark" id="navbar2">
-					<div class="container justify-content-center">
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo03">
-						<ul class="navbar-nav flex-column mr-auto mt-2 mt-lg-0 col-md-4">
-							<li class="nav-item  active">
-								<a class="nav-link foot-link" href="../html/financement.html">Contact <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link foot-link itemFoot" href="../html/financement.html">Numéro <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link foot-link itemFoot" href="../html/financement.html">Adresse <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link foot-link itemFoot" href="../html/financement.html">E-Mail <span class="sr-only">(current)</span></a>
-							</li>
-						</ul>
-						<ul class="navbar-nav flex-column mr-auto mt-2 mt-lg-0 col-md-4">
-							<li class="nav-item active">
-								<a class="nav-link foot-link" href="../html/financement.html">Mentions Légales<span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item ">
-								<a class="nav-link itemFoot foot-link" href="../html/financement.html">Conditions de vente <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item ">
-								<a class="nav-link itemFoot foot-link" href="../html/financement.html">Conditions d'utilisation <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item ">
-								<a class="nav-link itemFoot foot-link" href="../html/financement.html">RGPD<span class="sr-only">(current)</span></a>
-							</li>
-						</ul>
-						<ul class="navbar-nav flex-column mr-auto mt-2 mt-lg-0 col-md-4">
-							<li class="nav-item active">
-								<a class="nav-link foot-link" href="../html/financement.html">Contact <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item ">
-								<a class="nav-link itemFoot foot-link" href="../html/financement.html">Numéro <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link itemFoot foot-link" href="../html/financement.html">Adresse <span class="sr-only">(current)</span></a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link itemFoot foot-link" href="../html/financement.html">E-Mail <span class="sr-only">(current)</span></a>
-							</li>
-						</ul>
-					</div>
-					</div>
-				</nav>
-			</div>
+		    <nav class="navbar navbar-expand-lg navbar-dark" id="navbar2">
+		      <div class="container justify-content-center">
+		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+		        <span class="navbar-toggler-icon"></span>
+		      </button>
+		      <div class="collapse justify-content-center navbar-collapse" id="navbarTogglerDemo03">
+		        <ul class="navbar-nav flex-column mr-auto mt-2 mt-lg-0 col-md-4">
+		          <li class="nav-item  active">
+		            <a class="nav-link foot-link" href="../html/financement.html">Contact <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link foot-link itemFoot" href="../html/financement.html">Numéro <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link foot-link itemFoot" href="../html/financement.html">Adresse <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link foot-link itemFoot" href="../html/financement.html">E-Mail <span class="sr-only">(current)</span></a>
+		          </li>
+		        </ul>
+		        <ul class="navbar-nav flex-column mr-auto mt-2 mt-lg-0 col-md-4">
+		          <li class="nav-item active">
+		            <a class="nav-link foot-link" href="../html/financement.html">Mentions Légales<span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item ">
+		            <a class="nav-link itemFoot foot-link" href="../html/financement.html">Conditions de vente <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item ">
+		            <a class="nav-link itemFoot foot-link" href="../html/financement.html">Conditions d'utilisation <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item ">
+		            <a class="nav-link itemFoot foot-link" href="../html/financement.html">RGPD<span class="sr-only">(current)</span></a>
+		          </li>
+		        </ul>
+		        <ul class="navbar-nav flex-column mr-auto mt-2 mt-lg-0 col-md-4">
+		          <li class="nav-item active">
+		            <a class="nav-link foot-link" href="../html/financement.html">Contact <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item ">
+		            <a class="nav-link itemFoot foot-link" href="../html/financement.html">Numéro <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link itemFoot foot-link" href="../html/financement.html">Adresse <span class="sr-only">(current)</span></a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link itemFoot foot-link" href="../html/financement.html">E-Mail <span class="sr-only">(current)</span></a>
+		          </li>
+		        </ul>
+		      </div>
+		      </div>
+		    </nav>
+		  </div>
 	</footer>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	</body>
