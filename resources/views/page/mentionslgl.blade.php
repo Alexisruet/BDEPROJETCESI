268a702a01bd93a15e3fbd71c3fef8e2b839831e
Clément sanchez <clement.sanchez@viacesi.fr>
2019-01-22 15:09:31 +0100

    mentions lgl

--- resources/views/page/mentionslgl.blade.php
+++ resources/views/page/mentionslgl.blade.php
@@ -2,9 +2,66 @@
 
 @section('main')
 <h2>Mentions Légales</h2>
-<p>Nom : BDE-CESI-BDX </p>
-<p>Adresse : https://www.bde-cesi-bdx.fr/</p>
+<p>Nom : BDE-CESI-BDX <br>
+	Adresse : https://www.bde-cesi-bdx.fr/</p>
 <p>Ce site a été créé le 22/01/2019. Son auteur et responsable
-	de publication est joignable à cette adresse : clement.sanchez@viacesi.fr</p>
+	de publication est joignable à cette adresse : clement.sanchez@viacesi.fr
+</p>
+<p>Le Bureau des élèves du CESI de Bordeaux est une association à but non lucratifs <br>
+	 en charge de la vie associative de l’école. Le site met en place une boutique ainsi qu’une page <br>
+	 de discussion sur les évènements organisés. Les contenus qui y sont diffusés sont inclus dans <br>
+	 une base de données appartenant à Clément SANCHEZ. Vous ne pouvez ni reproduire <br>
+	 cette base de données, ni extraire des données contenues dans cette base autrement que pour votre <br>
+	 usage privé, conformément aux dispositions du Code de la propriété intellectuelle – Article L341-1.
+</p>
+<p>Le site internet est hébergé par la société MyHebergeur – 182 rue Barreyre 33300 Bordeaux. <br>
+	 Conformément à la loi, mes éléments d’identification personnelle lui ont été communiqués.<br>
+	 En cas de réclamations sur le contenu du site, sur les articles en ventes ou un commentaire <br>
+	 je vous saurez gré de m’adresser un courrier élèctronique.
+</p>
+<p>La loi vous permet même de vous adresser directement à l’hébergeur sous réserve de l’article
+	 6, I, 4° de la loi 2004-575 du 21 juin 2004 :</p>
+<p>Le fait, pour toute personne, de présenter aux [hébergeurs du site] un contenu ou une activité comme étant illicite <br>
+dans le but d’en obtenir le retrait ou d’en faire cesser la diffusion, alors qu’elle sait cette information inexacte, est puni <br>
+d’une peine d’un an d’emprisonnement et de 15 000 EUR d’amende.
+</p>
+<p>Le BDE-CESI-BDX autorise la mise en place d’un lien hypertexte pointant vers son contenu, sous réserve de ne pas utiliser <br>
+	 la technique du lien profond (deep linking), c’est-à-dire que les pages ne doivent pas être imbriquées à l’intérieur des pages <br>
+	 d’un autre site, mais accessible par l’ouverture d’une fenêtre ; mentionner la source BDE-CESI-BDX du contenu visé est obligatoire.<br>
+	 Cette autorisation ne s’applique pas aux sites Internet diffusant des informations à caractère polémique, pornographique, xénophobe <br>
+	 ou pouvant, dans une plus large mesure, porter atteinte à la sensibilité du plus grand nombre ou à l’ordre public.
+</p>
+<h3>Traitement des données, RGPD</h3>
+<p>Vos données ne seront jamais vendues, échangées ou partagées avec d’autre personnes physique ou moral. <br>
+ 	Vos données sont récupérées dans le but de pouvoir vous contacter en cas de besoin ainsi que pour faire des statistiques <br>
+  sur nos ventes, dans un souci d’optimisation des stocks. Lorsque vous participez à un événement organisé par nos soins nous sommes <br>
+	esponsables de ce qui s’y passe et devons donc détenir différentes informations à votre sujet en cas de problèmes.
+</p>
+<p>Les seules personnes ayant accès à vos données sont les administrateurs du site ainsi que <br>
+	les employés du CESI (qui ont déjà accès à vos informations personnelles grâce à votre dossier scolaire.) <br>
+	Vos informations sont conservées uniquement le temps de votre scolarité. <br>
+	La personne responsable de la protection de vos données est Clément SANCHEZ, joignable à l’adresse mail suivant, clement.sanchez@viacesi.fr
+</p>
+<h3>Conditions générales d'utilisation</h3>
+<p>En accédant à ce site internet, les internautes acceptent les présentes conditions générales d’utilisation.</p>
+<p>Le BDE-CESI-BDX met à la disposition des internautes une base de données d’informations pour lesquelles le maximum <br>
+	a été fait pour s’assurer de leur exactitude au moment de leur mise en ligne. Le BDE-CESI-BDX ne saurait être tenu <br>
+ 	pour responsable d’éventuelles erreurs ni d’éventuels préjudices liés à la consultation de ces informations.<br>
+	Le BDE-CESI-BDX se réserve le droit de modifier, corriger ou suspendre le contenu du site.
+</p>
+<p>Le BDE-CESI-BDX ne saurait être tenu responsable de l’utilisation qui serait faite par les internautes <br>
+	des informations contenues dans le blog. Des liens hypertextes présents sur le site https://www.BDE-CESI-BX.fr/ <br>
+	permettent l’accès à d’autres sites/blogs ... Sur lesquels le BDE-CESI-BDX n’exerce aucun contrôle. <br>
+	La responsabilité du BDE-CESI-BDX ne saurait en aucun cas être engagée en cas de préjudice causé par <br>
+	le contenu d’un ou plusieurs des sites/blogs … rattachés par un lien hypertexte.
+</p>
+<p>Les commentaires que l’on peut trouver au pied des billets reflètent uniquement l’avis de leurs auteurs. <br>
+	Ils sont modérés a posteriori. Tout commentaire à caractère injurieux, diffamatoire, xénophobe, raciste, <br>
+	antisémite, pornographique, révisionniste, homophobe, sexiste, d’incitation à la haine, à la violence ou en général <br>
+	tout sujet contraire à la loi et aux valeurs humanistes ne sont pas acceptés. Je me réserve le droit de supprimer <br>
+	sans avertissement tout commentaire dérogeant à cette règle.
+ </p>
+
+
 
 @endsection
