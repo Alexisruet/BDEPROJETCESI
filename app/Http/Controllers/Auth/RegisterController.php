058d212077d4fce040b732ae35be48985e20cd51
Alexis <alexis.ruet@viacesi.com>
2019-01-22 13:41:54 +0100

    n

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'surname' => $data['surname'],
             'email' => $data['email'],
-            'centre' => $data['centre'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'surname', 'email', 'centre', 'role', 'password',
+        'name', 'surname', 'email', 'password',
     ];
 
     /**
