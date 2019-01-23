e49439add9cc0da21fc10f946ccc23721334ef12
LouisPouget <louis.pouget@viacesi.fr>
2019-01-23 14:43:39 +0100

    tweet

--- resources/views/page/idea.blade.php
+++ resources/views/page/idea.blade.php
@@ -3,7 +3,7 @@
 @section('main')
 
 <div class="container idea">
-  <div class="card w-75">
+  <div class="card w-75" id="card">
     <div class="card-body row">
       <div class="col-md-2">
         <div class="roundedImage" style="background-color:red 0px 0px;"></div>
@@ -12,24 +12,20 @@
           <h5>Nom Prénom</h5>
           <p>@PrenomNom</p>
       </div>
+      <div class="col-md-5"> </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-light" id="more-infos">Plus d'infos</button>
+      </div>
     </div>
 
     <div class="text row">
       <div class="col-md-2"></div>
       <div class="col-md-9">
-        <h5 class="card-title">Oh ce serait une bonne idée d'oganiser un soccer un jeudi !</h5>
-      </div>
-
-    </div>
-
-    <div class="date row">
-      <div class="col-md-2"></div>
-      <div class="col-md-9">
-        <p class="card-text">11:24 - 23 Janv. 2019</p>
+        <h5 class="card-title">Oh ce serait une bonne idée d'organiser un soccer un jeudi !</h5>
       </div>
     </div>
 
-    <div class="likesNumber row">
+    <div class="likeNumber row">
       <div class="col-md-2"></div>
       <div class="col-md-9">
         <p class="card-text">11:24 - 23 Janv. 2019</p>
@@ -41,18 +37,15 @@
       <div class="col-md-9 row" id="logosLikeBar">
         <div class="col-md-2">
           <img src="img/comment.png" alt="comment">
-
         </div>
         <div class="col-md-2">
           <img src="img/retweet.png" alt="retweet">
         </div>
         <div class="col-md-2">
-          <img src="img/like.png" alt="like">
+          <img src="img/like.png" alt="like">&nbsp21
         </div>
       </div>
     </div>
 
   </div>
 </div>
-
-@endsection
