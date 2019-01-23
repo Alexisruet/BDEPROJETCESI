ccb9e24326f32aac8476a0a03e0cd3e99729db80
LouisPouget <louis.pouget@viacesi.fr>
2019-01-23 13:26:05 +0100

    yes

--- resources/views/page/idea.blade.php
+++ resources/views/page/idea.blade.php
@@ -2,9 +2,56 @@
 
 @section('main')
 
-<div class="container">
-      
-				<h2>YO la street</h2>
+<div class="container idea">
+  <div class="card w-75">
+    <div class="card-body row">
+      <div class="col-md-2">
+        <div class="roundedImage" style="background-color:red 0px 0px;"></div>
+      </div>
+      <div id="tag">
+          <h5>Nom Prénom</h5>
+          <p>@PrenomNom</p>
+      </div>
     </div>
+
+    <div class="text row">
+      <div class="col-md-2"></div>
+      <div class="col-md-9">
+        <h5 class="card-title">Oh ce serait une bonne idée d'oganiser un soccer un jeudi !</h5>
+      </div>
+
+    </div>
+
+    <div class="date row">
+      <div class="col-md-2"></div>
+      <div class="col-md-9">
+        <p class="card-text">11:24 - 23 Janv. 2019</p>
+      </div>
+    </div>
+
+    <div class="likesNumber row">
+      <div class="col-md-2"></div>
+      <div class="col-md-9">
+        <p class="card-text">11:24 - 23 Janv. 2019</p>
+      </div>
+    </div>
+
+    <div class="likebar row">
+      <div class="col-md-2"></div>
+      <div class="col-md-9 row" id="logosLikeBar">
+        <div class="col-md-2">
+          <img src="img/comment.png" alt="comment">
+        </div>
+        <div class="col-md-2">
+          <img src="img/retweet.png" alt="retweet">
+        </div>
+        <div class="col-md-2">
+          <img src="img/like.png" alt="like">
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 @endsection
