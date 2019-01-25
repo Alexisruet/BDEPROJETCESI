78a470169062123404358c41c3a042ffa00c2693
Clément sanchez <clement.sanchez@viacesi.fr>
2019-01-25 09:27:59 +0100

    page create IDea

--- resources/views/page/createidea.blade.php
+++ resources/views/page/createidea.blade.php
@@ -1,25 +1,25 @@
 @extends('template')
 
 @section('main')
-<div class="container" id="cIdea">
-	<div class="row">
-      <form class="col-md-12" action="{{ route('createIdea') }}" method='POST' id = 'form'>
-                {{csrf_field()}}
-      	<div class="col-md-5" id="titreCI">
+	<div class="contain-cIdea row" id="bodyCI">
+		<div class="col-md-4">
+
+		</div>
+		<div class="col-md-3" id="yopLaBoum">
+			<form class="" action="{{ route('createIdea') }}" method='POST' id = 'form'>
+								{{csrf_field()}}
+				<div class="" id="titreCI">
 					<label for="title">Titre de votre idée :</label>
 					<input placeholder="Titre de l'idée" class="form-control" type='text' name='title' id='title' required></input>
 				</div>
-					<div class="col-md-5">
+					<div class="">
 						<div class="input-field">
 							<label for="exampleFormControlTextarea1" id="desCI">Description de votre idée :</label>
-            	<textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Décrire votre idée" name='description' form='form' required></textarea>
-            </div>
-            <button class="waves-effect waves-light btn" id="ideaValidate" type='submit'>Valider</button>
-          </div>
-    </form>
-
+							<textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Décrire votre idée" name='description' form='form' required></textarea>
+						</div>
+						<button class="waves-effect waves-light btn" id="ideaValidate" type='submit'>Valider</button>
+					</div>
+		</form>
+		</div>
 	</div>
-
-</div>
-	<img src="img/lumen.jpg" alt="des ampoules">
 @endsection
