4d775c2a6236121633eb4b22a5be150bdece2e11
LouisPouget <louis.pouget@viacesi.fr>
2019-01-28 18:15:06 +0100

    checkbox cgv

--- public/js/validation.js
+++ public/js/validation.js
@@ -19,3 +19,12 @@ function Validation() {
         }
     }
 }
+
+function checkForm(form) {
+    if (!form.terms.checked) {
+        alert("Please indicate that you accept the Terms and Conditions");
+        form.terms.focus();
+        return false;
+    }
+    return true;
+}
--- resources/views/page/shopping-cart.blade.php
+++ resources/views/page/shopping-cart.blade.php
@@ -4,35 +4,50 @@
 
 <div class="container" id="shopie">
 
-  <h2>Panier {{ Session::has('cart') ? Session::get('cart')->totalQty : '' }}</h2>
-@foreach($products as $product)
+    <h2>Panier {{ Session::has('cart') ? Session::get('cart')->totalQty : '' }}</h2>
+    @foreach($products as $product)
 
-  <ul class="list-group">
-    <li class="list-group-item col-md-12" >
-      <div class="row">
+    <ul class="list-group">
+        <li class="list-group-item col-md-12">
+            <div class="row">
 
-        <div class="col-md-2">
-          <strong>Nom : {{ $product['item']['name'] }}</strong>
-        </div>
-        <div class="col-md-2">
-          <span class="label label-success">Prix : {{ $product['price'] }}</span>
-        </div>
-        <div class="col-md-2">
-          <span class="label label-succes">Quantité : {{ $product['qty'] }}</span>
-        </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-2">
-          <a href="{{ route('product.remove', ['id' => $product['item']['id']]) }}">
-            <button><i class="fas fa-times"></i>&nbspSupprimer</button>
-          </a>
-        </div>
+                <div class="col-md-2">
+                    <strong>Nom : {{ $product['item']['name'] }}</strong>
+                </div>
+                <div class="col-md-2">
+                    <span class="label label-success">Prix : {{ $product['price'] }}</span>
+                </div>
+                <div class="col-md-2">
+                    <span class="label label-succes">Quantité : {{ $product['qty'] }}</span>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-2">
+                    <a href="{{ route('product.remove', ['id' => $product['item']['id']]) }}">
+                        <button><i class="fas fa-times"></i>&nbspSupprimer</button>
+                    </a>
+                </div>
 
-      </div>
-    </li>
-  </ul>
+            </div>
+        </li>
+    </ul>
 
-@endforeach
-<button class="waves-effect waves-light btn" id="ideaValidate" type='submit'>Commander</button>
+    @endforeach
+    <form class="form-group row" onsubmit="return checkForm(this);">
+        <div class="col-md-4">
+            <a href="https://www.paypal.me/jokerSanch"><button class="waves-effect waves-light btn" id="ideaValidate" type='submit'>Commander</button></a>
+        </div>
+        <div class="col-md-4">
+            <label class="custom-control custom-checkbox">
+                <input class="custon-control-input" type="checkbox" name="cgv" value="accepted" required>
+                <span class="custom-control-indicator"></span>
+                <span class="custom-control-description">J'accepte les <a href="mentionslgl#cgv">Conditions générales de vente</a></span>
+            </label>
+    </form>
+
+</div>
+
+
+
 </div>
 
 @endsection
