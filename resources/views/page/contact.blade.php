f70ff67282002af4e8c1aff9f216a9da9c3f42c1
LouisPouget <louis.pouget@viacesi.fr>
2019-01-28 09:07:52 +0100

    cc

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -31,9 +31,10 @@
       <div class="col-md-10">
         <h5>264 Boulevard Godard, 33300 Bordeaux </h5>
       </div>
-      <div class="col-md-10">
+      <div class="col-md-12">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2827.8711132168805!2d-0.5798439844625093!3d44.86491857909836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54d679fc16d979%3A0x53db14bce1eb09fc!2sCampus+CESI!5e0!3m2!1sfr!2sfr!4v1548604633877" width="600" height="450" frameborder="0" style="border:0" allowfullscreen id="mapGg"></iframe>
       </div>
+
     </div>
 
   </div>
