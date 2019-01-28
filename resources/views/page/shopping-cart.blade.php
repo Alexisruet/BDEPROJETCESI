435140e4a0159646c4ee1bf2df6d9257d3898b51
LouisPouget <louis.pouget@viacesi.fr>
2019-01-28 18:22:33 +0100

    checkbox cgv marche bien

--- resources/views/page/shopping-cart.blade.php
+++ resources/views/page/shopping-cart.blade.php
@@ -32,16 +32,17 @@
     </ul>
 
     @endforeach
-    <form class="form-group row" onsubmit="return checkForm(this);">
+    <form class="form-group row" action="https://www.paypal.me/jokerSanch">
         <div class="col-md-4">
             <a href="https://www.paypal.me/jokerSanch"><button class="waves-effect waves-light btn" id="ideaValidate" type='submit'>Commander</button></a>
         </div>
         <div class="col-md-4">
             <label class="custom-control custom-checkbox">
-                <input class="custon-control-input" type="checkbox" name="cgv" value="accepted" required>
+                <input class="custon-control-input" type="checkbox" name="cgv" required>
                 <span class="custom-control-indicator"></span>
                 <span class="custom-control-description">J'accepte les <a href="mentionslgl#cgv">Conditions générales de vente</a></span>
             </label>
+        </div>
     </form>
 
 </div>
