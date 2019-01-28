4167ac58b06ad4509edd6f3444bdc75c9f3e98e9
LouisPouget <louis.pouget@viacesi.fr>
2019-01-28 22:23:12 +0100

    bouton déplace

--- resources/views/page/futurevent.blade.php
+++ resources/views/page/futurevent.blade.php
@@ -1,36 +1,39 @@
 @extends('template')
 
 @section('main')
-
+<?php
+	$user = Auth::user()->id_grade;
+?>
+<div class="col-md-1">
+	@if($user == 3)
+	<a href="event"><button type="button" class="btn btn-light" id="createEvent">
+			Add event
+		</button></a>
+</div>
 <div class="container">
 	<div class="row">
-			@foreach ($events as $event)
-			<div class="idea card bg-light col-md-5">
-				<div class="card-body">
-					<h3>{{ $event->title }}</h3>
-					<p>{{ $event->description }}</p>
-          @foreach($pictures as $picture)
-          	@if($picture->id_event == $event->id)
-							<div class="imgWei">
-								<img class="card-img-top d-block" src="img/{{ $picture->url }}" height="250" width="100" alt="photo de l'event">
-							</div>
-						@endif
-					@endforeach
-  				<button type="submit" class="btn link btn-dark" id="inscription">
-						<a href="/events/{{ $event -> id}}">Pour s'inscrire</a>
-					</button>
-				</div>
-			</div>
-			<div class="col-md-1"></div>
 
-			@endforeach
-			<?php
-				$user = Auth::user()->id_grade;
-			?>
-			@if($user == 3)
-				<a href="event"><button type="button" class="btn btn-light" id="createEvent" >
-					Add event
-				</button></a>
-			@endif
+		@endif
+		@foreach ($events as $event)
+		<div class="idea card bg-light col-md-5">
+			<div class="card-body">
+				<h3>{{ $event->title }}</h3>
+				<p>{{ $event->description }}</p>
+				@foreach($pictures as $picture)
+				@if($picture->id_event == $event->id)
+				<div class="imgWei">
+					<img class="card-img-top d-block" src="img/{{ $picture->url }}" height="250" width="100" alt="photo de l'event">
+				</div>
+				@endif
+				@endforeach
+				<button type="submit" class="btn link btn-dark" id="inscription">
+					<a href="/events/{{ $event -> id}}">Pour s'inscrire</a>
+				</button>
+			</div>
+		</div>
+		<div class="col-md-1"></div>
+
+		@endforeach
+
 	</div>
 	@endsection
